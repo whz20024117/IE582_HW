49fd6b4f6de31d51fca03bec5d82ead4f4b91e0c
cocowang <whz20024117@gmail.com>
2019-12-04 15:56:39 -0500

    Project models done

---
+++ Project/models.py
@@ -0,0 +1,69 @@
+import tensorflow as tf
+import numpy as np
+from keras.preprocessing.text import tokenizer_from_json
+from tensorflow.keras import Model
+from tensorflow.keras.layers import LSTM, Dense, TimeDistributed, Bidirectional, Input, Embedding
+import json
+
+
+# Some helper functions
+def load_bow(path='./data/bow.json'):
+    with open('./data/bow.json') as f:
+        _bow = json.load(f)
+    bow = tokenizer_from_json(_bow)
+
+    return bow
+
+
+def pad_seq(seqs, max_len):
+    tmp = np.zeros(shape=(len(seqs), max_len), dtype=np.float)
+
+    for idx, seq in enumerate(seqs):
+        if len(seq) > max_len:
+            seq = seq[:max_len]
+        tmp[idx, :min(len(seq), max_len)] = seq
+
+    return tmp
+
+
+class FakeNewsModel:
+    def __init__(self, bow, lstm_units, fc_units, emb_sz=256, max_len=30, lr=0.001):
+        self.lstm_units = lstm_units
+        self.bow = bow
+        self.fc_units = fc_units
+        self.emb_sz = emb_sz
+        self.vocab_sz = len(bow.word_index)
+        self.max_len = max_len
+        self.model = self._build()
+        self.model.compile(optimizer=tf.keras.optimizers.Adam(lr),
+                           loss=tf.keras.losses.BinaryCrossentropy())
+        self.model.summary()
+
+    def _build(self):
+        x = Input(shape=(self.max_len,))
+        out = Embedding(self.vocab_sz + 1, self.emb_sz, mask_zero=True)(x)
+        # LSTM layers
+
+        if len(self.lstm_units)>1:
+            for p in self.lstm_units[:-1]:
+                out = LSTM(p, return_sequences=True, activation='relu')(out)
+            out = LSTM(self.lstm_units[-1], activation='relu')(out)
+        elif len(self.lstm_units)==1:
+            out = LSTM(self.lstm_units[-1], activation='relu')(out)
+
+        # out = LSTM(self.lstm_units[-1], activation='relu')(out)
+
+        # FC layers
+        for p in self.fc_units:
+            out = Dense(p, activation='relu')(out)
+
+        out = Dense(1, activation='sigmoid')(out)
+
+        return Model(x, out)
+
+    def train(self, data, label, batch_sz=32, epochs=20):
+        x = self.bow.texts_to_sequences(data)
+        x = pad_seq(x, self.max_len)
+        y = np.array(label, dtype=np.float)
+
+        self.model.fit(x, y, batch_size=batch_sz, epochs=epochs)
---
+++ Project/preprocess.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from urllib.parse import urlparse
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+import json
+
+
+def main():
+    data = pd.read_csv('data/data.csv')
+    data['agent'] = data['URLs'].apply(lambda x: urlparse(x)[1])
+
+    headlines = data['Headline'].tolist()
+    labels = data['Label'].tolist()
+
+    BOW = Tokenizer()
+    BOW.fit_on_texts(headlines)
+
+    # Save Bag of Word
+    with open('./data/bow.json', 'w') as f:
+        json.dump(BOW.to_json(), f)
+
+    return
+
+
+if __name__ == '__main__':
+    main()
